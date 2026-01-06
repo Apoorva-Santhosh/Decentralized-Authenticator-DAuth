dd67f3063099c0865d243a2810d3dbb3bf9fa233
Apoorva Santhosh <apoorvas2040@gmail.com>
2026-01-06 18:55:57 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Decentralized-Authenticator-DAuth/',
+  base: '/BlockAuth-PassAnalysis/',
   plugins: [react()],
   server: {
     port: 5173
