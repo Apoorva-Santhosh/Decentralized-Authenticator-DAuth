60465e2f5c870901671b2ffce2971aba48a73f06
Apoorva Santhosh <apoorvas2040@gmail.com>
2026-01-06 18:33:02 +0530

    Deployment changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@contracts': path.resolve(__dirname, 'artifacts/contracts'),
-    },
+  server: {
+    port: 5173
   },
+  build: {
+    outDir: 'dist'
+  }
 });
